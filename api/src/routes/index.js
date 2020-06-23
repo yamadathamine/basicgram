3f7068b707b9db12586212ce0b0a0988859e8e6f
Thamine Yamada <thamineyamada@gmail.com>
2020-06-23 18:21:44 -0300

    parte 1 feita

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express with TypeScript' });
+  res.render('index', { title: 'Thami' });
 });
 
 module.exports = router;
